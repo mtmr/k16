88d198a99fa86664365ed071ca92e5188c4ff7ff
Muthu Raj <muthurajmuthu15@googlemail.com>
2015-05-25 00:44:56 +0530

    Added validation

--- php/check.php
+++ php/check.php
@@ -1,8 +1,8 @@
 <!doctype html>
 <head>
 <!-- Bootstrap CSS and JS -->
-<!-- Saves reinventing the wheel and takes care of basic validation , Besides, no regex is perfect-->
-<!--link rel="stylesheet/css" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.4/paper/bootstrap.min.css"-->
+<!-- HTML5 is the ~~boilerplate~~ for now -->
+<!-- Validation Added -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.4/cosmo/bootstrap.min.css" rel="stylesheet">	
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"> </script>
@@ -12,22 +12,22 @@
 	<!-- Form Begins -->
 	<h1 class="text-center"> Enter your details </h1>
 	<div class="container col-md-6 col-md-offset-3">
-	<form action="my_details.php" method="POST">
+	<form action="my_details.php" onsubmit="return validate()" method="POST">
 	<fieldset>
 		<div class="form-group">	
-		<input type="text" class="form-control" name="name" placeholder="Your Name" style="cursor: auto;" required pattern="..+">
+		<input type="text" class="form-control" name="name" id="name" placeholder="Your Name" style="cursor: auto;" required pattern="..+">
 		</div>
 		<div class="form-group">
-			<input type="email" class="form-control" name="email" placeholder="Your Email" style="cursor: auto;" required pattern=".+@.+">
+			<input type="email" class="form-control" name="email" id="email" placeholder="Your Email" style="cursor: auto;" required pattern=".+@.+">
 		</div>
 		<div class="form-group">
-			<input type="url" class="form-control" name="github" placeholder="Your Github Profile" style="cursor: auto;">
+			<input type="text" class="form-control" name="github" id="github" placeholder="Your Github Profile" style="cursor: auto;">
 		</div>
 		<div class="form-group">
-			<input type="url" class="form-control" name="facebook" placeholder="Your FB Profile" style="cursor: auto;">
+			<input type="text" class="form-control" name="facebook" id="facebook" placeholder="Your FB Profile" style="cursor: auto;">
 		</div>
 		<div class="form-group">
-			<input type="text" class="form-control" name="website" placeholder="Website" style="cursor: auto;">
+			<input type="text" class="form-control" name="website" id="website" placeholder="Website" style="cursor: auto;">
 		</div>
                  
    		<div class="form-group">
@@ -39,16 +39,27 @@
   	</div>
 	<!-- Form Ends -->
 	<!--Validating the input-->
-	<!--script type="text/javascript"">
-	function validate()
-	{
-		//TODO: Update to Manual Validation
-		var name=document.getElementbyID("name").value;
-		var email=document.getElementbyID("email").value;
-		var github=document.getElementbyID("github").value;
-		var facebook=document.getElementbyID("facebook").value;
-		var website=document.getElementbyID("website").value;
-	}
-	</script-->
+	<script type="text/javascript">
+	function validate() {
+        //Client side validation using JS
+        window.alert("Debugging");
+        var name = document.getElementById("name").value;
+        var email = document.getElementById("email").value;
+        var github = document.getElementById("github").value;
+        var facebook = document.getElementById("facebook").value;
+        var website = document.getElementById("website").value;
+        if (!(name.length))
+            window.alert("Please enter a valid name");
+        emailreg = /\w+@.\w+\.\w+/; //Blame ICANN for the crazy TLDs
+        if (!emailreg.test(email))
+            window.alert("Please enter a valid email");
+        else 
+        	console.log("Hi");
+        /*githubreg = /github.com\/.\w+/;
+        if (!githubreg.test(github))
+            window.alert("Please enter a valid github URL");*/ // TODO: Pad with github.com URL automatically if user enters just the username --Done in Server side
+        //FB and Website are non-essential. We will take care of that in the server side using PHP
+    }
+	</script>
 </body>
 </html>
--- php/my_details.php
+++ php/my_details.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <head>
 <!-- Bootstrap CSS and JS -->
-<!-- takes care of basic validation , Besides, no regex is perfect-->
+<!--HTML5 takes care of basic validation , Besides, no regex is perfect-->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.4/paper/bootstrap.min.css"-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"> </script>
@@ -9,6 +9,7 @@
 </head>
 <body>
 <div class="container col-md-6 col-md-offset-3">
+<h3 class="text-center">Your Details</h3>
 <table class="table table-striped table-hover">
   <thead>
   <tr>
@@ -20,11 +21,43 @@
   <?php
  	foreach($_POST as $key => $value)
 	{
-	  echo "<tr>";
-	  echo "<td>".$key."</td>";
-	  echo "<td>".$value."</td>";
-	  echo "</tr>";
+    $details[$key]=$value;
 	}
+  $name = trim($details['name']); //Remove Extraneous White space
+  $email = trim($details['email']);
+  $facebook = trim($details['facebook']);
+  //We want to display the FB url as a Link
+  if(!stripos($facebook, "facebook.com") || !stripos($facebook, "fb.com")) // Don't forget to check for fb.com
+    $facebook = "http://facebook.com/" . $facebook;
+  else
+    $facebook = "http://".$facebook;
+  $github = trim($details['github']);
+  if(!stripos($github, "github.com"))
+    $github = "http://github.com/" . $github;
+  else
+    $github = "http://".$github;
+  $website = "http://".$details['website'];
+  //TODO: Make an array and loop through them instead of this 
+  echo "<tr>";
+  echo "<td> Name</td>";
+  echo "<td>".$name."</td>" ;
+  echo "</tr>";
+  echo "<tr>";
+  echo "<td> Email</td>";
+  echo "<td>".$email."</td>" ;
+  echo "</tr>";
+  echo "<tr>";
+  echo "<td> Facebook</td>";
+  echo "<td> <a href=\"".$facebook."\"> Facebook</a></td>" ;
+  echo "</tr>";
+  echo "<tr>";
+  echo "<td> Github</td>";
+  echo "<td> <a href=\"".$github."\"> Github Profile</a></td>" ;
+  echo "</tr>";
+  echo "<tr>";
+  echo "<td> Email</td>";
+  echo "<td> <a href=\"".$website."\"> Website</a></td>" ;
+  echo "</tr>";
 ?>
   </tbody>
 </table>
